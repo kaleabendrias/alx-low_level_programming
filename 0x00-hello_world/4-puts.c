aba1f25cc99937fbeb4349ed786598141b9360f2
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-16 04:20:17 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - Prints "Programming is like building a multilingual  
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
+  
   return(0);
 }
