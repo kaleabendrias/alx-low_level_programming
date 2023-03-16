4423efd3a13c33600c6459ba10f4a510d049ead4
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-16 04:28:26 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/* number 5 */
+
+#include <stdio.h>
+
+/* main prints into the program */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+
+  return(0);
+}
