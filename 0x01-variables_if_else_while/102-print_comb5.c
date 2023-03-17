51edff64af409ee3c879bee22c0e4a21dd7949c7
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-17 08:27:18 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+/* number 10 */
+#include <stdio.h>
+
+/**
+ * main- this is where the main starts
+ * executing.
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 98; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 98 && y == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
