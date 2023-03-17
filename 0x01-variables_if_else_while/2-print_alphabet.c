a112014314f345d40a12c4d923b1ec33eb175f09
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-17 00:40:32 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/* number 2 */
+#include <stdio.h>
+
+/**
+ * main- this is where the main starts
+ * executing.
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+
+	return (0);
+}
+
