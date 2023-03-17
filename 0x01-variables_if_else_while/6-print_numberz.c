9650b74284d3889767d9402863cba6e35239e1c5
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-17 02:57:47 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/* number 6 */
+#include <stdio.h>
+
+/**
+ * main- this is where the main starts
+ * executing.
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 48; i <= 58; i++)
+	{
+		putchar(i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
