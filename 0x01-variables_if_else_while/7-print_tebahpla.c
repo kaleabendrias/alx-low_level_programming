f9e59d6a44ebf3bb14bf3447c405443b0c98b873
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-17 03:15:39 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+/* number 7 */
+#include <stdio.h>
+
+/**
+ * main- this is where the main starts
+ * executing.
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
