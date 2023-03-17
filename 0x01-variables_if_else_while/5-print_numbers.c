03c4c73d564cb50fa3c86b4623f86ac944d6cc40
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-17 02:39:43 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/* number 5 */
+#include <stdio.h>
+
+/**
+ * main- this is where the main starts
+ * executing.
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+
+	return (0);
+}
+
