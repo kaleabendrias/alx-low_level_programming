3dc32c4d90c795c744892630230b4aecc0b4d20e
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-21 04:18:33 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds twon int.
+ * @x: The first number.
+ * @y: The second number.
+ * Return: the sum of the two numbers.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
