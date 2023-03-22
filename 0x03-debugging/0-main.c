691d69b8ef9ee317c6bd496c8b0c53b2a60c18bb
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-21 23:32:58 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _abs - calculate the absolute value of the number from zero.
+ * @i: the integer to be compared.
+ * Return: nothing is returned.
+ */
+
+void pos(int i)
+{
+	if (n > 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if ( n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else
+		printf("%d is zero" n);
+}
+
