05bc962f82a965d8aa0863ab3b092ba731c03830
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-23 03:38:02 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: placeholder for the first number.
+ * @b: placeholder for the second number.
+ * Return: Always succes.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
