e37da53c10017c4c3c24c5be00e975b146bd4fe8
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-23 04:44:53 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: palceholder for printing upto n times.
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_puthcar('-');
+		}
+		_putchar('\n');
+	}
+}
+
