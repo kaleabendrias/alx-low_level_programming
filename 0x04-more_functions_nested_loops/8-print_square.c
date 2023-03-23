eb79d8424073b57e584c4df77a4b91263b36d69f
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-23 05:50:19 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "math.h"
+
+/**
+ * print_square - prints the char # as much as size.
+ * @size: placeholder
+ * Return: void.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for( i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
+
