b85d078f0a85c7ed6fa8b3515a8c7bb83eac7042
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-23 05:27:06 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal line.
+ * @n: place holder for number of times to draw/.
+ * Return: void.
+ */
+
+void print_diagonal(void)
+{
+	int x;
+	int y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x <=n; x++)
+		{
+			for (y = 0; y <=x; y++)
+			{
+				_putchar(92);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
+
