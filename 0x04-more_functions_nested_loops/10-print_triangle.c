f0d1f491e097ffa99bd617bf7d9ce3c85b7c2d0e
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-23 08:08:33 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,13 +18,13 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size - i; j++)
+			for (j = size - i; j > 1; j--)
 			{
-				_putchar(' ');
+				_putchar(32);
 			}
 			for (k = 0; k <= i; k++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
 			_putchar('\n');
 		}
