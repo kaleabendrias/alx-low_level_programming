d19a30729c8fee1c38b8d1c3923b8ef29e20aa38
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-23 07:48:08 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - start of code.
+ * @size: place holder for the number of lines.
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i; k++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
+
