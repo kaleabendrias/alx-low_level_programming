4c005d60ba6238fc40fbf130f2b2bdd5a009d3d9
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-23 04:55:41 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,17 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('-');
+			_putchar(95);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
