c7395a201a9cb0cd33bc3bf9809badc1828848cd
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-27 01:08:38 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string.
+ * @s: placeholder for the string.
+ * Return: none.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char temp;
+	int len;
+
+	len = strlen(s);
+	j = len - 1;
+	for (i = 0; i <= (len - 1) / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		j--;
+	}
+	_putchar('\n');
+}
+
