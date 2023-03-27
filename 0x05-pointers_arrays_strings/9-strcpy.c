249a24649ebbc885a9b71c81cc584924607b3fd2
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-27 03:58:17 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,11 +5,12 @@
  * *_strcpy - copys string to other var.
  * @dest: placeholder.
  * @src: placeholder.
+ * Return: an int dest.
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
-	return dest;
+	return (dest);
 }
 
