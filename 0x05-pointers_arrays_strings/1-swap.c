4dadf3190d129e6ca78b3078d08b651957a70efc
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-26 23:51:50 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of two intigers.
+ * @a: placeholder.
+ * @b: placeholder
+ * Return: none
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
