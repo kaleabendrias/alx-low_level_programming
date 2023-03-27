1b61e993bdce2d6b505556910e808b339c0cf7bb
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-27 00:05:52 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of string.
+ * @s: placeholder for variable.
+ * Return: length of string.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
