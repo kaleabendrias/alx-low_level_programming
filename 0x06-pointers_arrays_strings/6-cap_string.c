26f1b9d80c3adf062d9e320ce000ef42360e5ae5
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 09:53:04 -0700

    capitalize sentence

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes paragraph correctly.
+ * @str: placeholder.
+ * Return: String.
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '.')
+		{
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
