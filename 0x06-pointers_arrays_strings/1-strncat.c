5798f7e4b20da1cf4f78fea0ee02bd102e5340ab
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 08:24:53 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,26 +11,17 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int len1;
-	int len2;
 	int i;
 
 	len1 = 0;
-	len2 = 0;
 
 	while (dest[len1] != '\0')
 	{
 		len1++;
 	}
-	while (src[len2] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		len2++;
-	}
-	for (i = 0; i < n; i++)
-	{
-		if (n < len2)
-		{
-			dest[i + len1] = src[i];
-		}
+		dest[i + len1] = src[i];
 	}
 	return (dest);
 }
