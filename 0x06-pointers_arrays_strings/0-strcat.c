696f7bcf21dab91a609ef00299c13fe89d750dca
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 08:11:06 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,14 @@ char *_strcat(char *dest, char *src)
 	int len;
 	int i;
 
+	len = 0;
 	while (dest[len] != '\0')
 	{
 		len++;
 	}
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[len + i] = str[i];
+		dest[len + i] = src[i];
 	}
 	return (dest);
 }
