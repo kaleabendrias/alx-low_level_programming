109c003b72717b5b7fc7610d1776249909d4752b
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 12:20:06 -0700

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * leet - turns some char into number.
+ * @str: place holder
+ * Return: string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	for(int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+		else
+		{
+			str[i] = str[i];
+		}
+	}
+	return (str);
+}
+
