d8f3f2094be4bace171b46f46d4e4f3f225e1afd
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 02:00:49 -0700

    2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strncat - does act like strcat but
+ * @n: bytes
+ * @dest: place holder for first string
+ * @src: place holder for second string
+ * Return: return dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1;
+	int len2;
+	int i;
+	int j;
+
+	len1 = 0;
+	len2 = 0;
+	j = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len++;
+	}
+	while (src[len2] != '\0')
+	{
+		len2++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (n < len2)
+		{
+			dest[i + len1] = src[i];
+		}
+	}
+	return (dest);
+}
+
