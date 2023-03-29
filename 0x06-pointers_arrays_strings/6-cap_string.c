e1961cc5dd2133c3c5c7cc5e7b2792657f204dc9
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 10:51:24 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,6 @@ char *cap_string(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == '\t')
-		{
-			str[i] = ' ';
-		}
 		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '.'
 				|| str[i - 1] == '\n' || str[i - 1] == '\t'
 				|| str[i - 1] == ',' || str[i - 1] == ';'
