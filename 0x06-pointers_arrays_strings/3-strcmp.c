73d25aaa9f8398cf5e4edf396c6d7975aa761b1c
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 08:32:11 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	return (s1[0] - s2[0]);
+	int i;
+	
+	while (s1[i] != '\0' && s2[i] != ['\0'])
+	{
+		if (s1[i] != s2[i])
+			return s1[i] - s2[i];
+		i++;
+	}
+	return s1[i] - s2[i];
 }
 
