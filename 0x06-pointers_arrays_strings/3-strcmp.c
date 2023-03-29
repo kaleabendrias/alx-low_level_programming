64179b74c59b772b7a0c9ba0a29db45a728dfa40
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-29 05:27:51 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that does the work of strcmp.
+ * @s1: placeholder
+ * @s2: placeholder.
+ * Return: intiger
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (s1[0] - s2[0]);
+}
+
