9c3cedc157ba6d3d158af9c0d4e2ceb2ea81c3a9
kaleabendrias <kaleabendrias1212@gmail.com>
2023-03-31 06:41:05 -0700

    print num

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,6 +9,7 @@
 void print_number(int n)
 {
 	char neg;
+	unsigned int x;
 
 	neg = 45;
 	if (n < 0)
@@ -16,10 +17,11 @@ void print_number(int n)
 		_putchar(neg);
 		n = -n;
 	}
-	if (n / 10)
+	x = n;
+	if (x / 10)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(x % 10 + '0');
 }
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,3 +18,5 @@ char *leet(char *str);
 
 void print_number(int n);
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
