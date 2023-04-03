6b494c4b7762bc416a3a856b8792ea14a6fb5bee
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-03 06:25:15 -0700

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagsums - adds main and secondary diagonal.
+ * @a: ptr.
+ * @size: size of the in.
+ * Return: none.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum1, i, j;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 = *(a + i * size + j);
+			}
+			if (i == size - j - 1)
+			{
+				sum2 = *(a + i * size + j);
+			}
+		}
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}
+
