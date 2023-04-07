5cabcba7d28acf2732a50ccffd0cd475317b721e
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-07 13:29:29 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		char *endptr;
 		long num = strtol(argv[i], &endptr, 10);
 
-		if (*endptr != '\0' || num == 0 && *argv[i] != '0')
+		if (*endptr != '\0' || (num == 0 && *argv[i] != '0'))
 		{
 			printf("Error\n");
 			return (1);
