1618445c1750552b1fb4f64d5bb71225e47be44a
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-06 22:45:36 -0700

    argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - shows the first argument.
+ * argc: count of the passed arguments.
+ * argv: array of strings.
+ * Return: 0.
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}
+
