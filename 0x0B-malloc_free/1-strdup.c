1ccf2329b556db63f3525cbb87c866bd2751b4c0
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-10 00:13:01 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,10 +10,17 @@
 
 char *_strdup(char *str)
 {
-	unsigned int i;
-	char *s;
+	unsigned int i, size;
+	char *s, *k;
 
-	s = malloc(sizeof(char));
+	size  = 0;
+	*k = str;
+	while (*k != '\0')
+	{
+		size++;
+		k++;
+	}
+	s = malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
