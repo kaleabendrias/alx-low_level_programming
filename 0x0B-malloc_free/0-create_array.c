b8d609833890ca5e465ffbb274fa47c0b74df4ce
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-09 23:19:54 -0700

    malloc that allocate char into memo.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - initializes allocated memory wih char.
+ * @size: memory size.
+ * @c: charcter to be placed in memo.
+ * Return: Always.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size  == 0)
+		return (0);
+	arr = (char *) malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+		*(arr + i) = c;
+	return (arr);
+}
+
