cb78000c1fc0d76ea0ad040e5a67f93ff881a222
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-10 01:42:57 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,16 +30,16 @@ char *str_concat(char *s1, char *s2)
 	}
 	s = malloc((size1 + size2 + 1) * sizeof(char));
 	p = s;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (*s1 != '\0')
 	{
-		if (s1 == NULL)
-			s1 = "";
 		*p++ = *s1++;
 	}
 	while (*s2 != '\0')
 	{
-		if (s2 == NULL)
-			s2 = "";
 		*p++ = *s2++;
 	}
 	*p = '\0';
