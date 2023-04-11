203d6b165cb8c04152fdcd212b71638d954adae5
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-10 22:09:20 -0700

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * argstostr - prints strings in a newline.
+ * @ac: placeholder.
+ * @av: placeholder.
+ * Return: Always.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int len, k, i, n;
+	char *s;
+
+	len = 0;
+	k = 0;
+	if (ac == 0 || av ==NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
+	s = malloc(sizeof(char) * len);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			s[k++] = av[i][n];
+		if (s[k] == '\0')
+			s[k++] = '\n';
+	}
+	return (s);
+}
+
