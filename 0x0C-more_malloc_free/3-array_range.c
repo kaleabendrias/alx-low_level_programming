a1a37de8554868446f0ca7c21893d8e42b5f9fd3
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-12 01:30:30 -0700

    array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * array_range - create memo for array of intigers.
+ * @min: starting point.
+ * @max: end point.
+ * Return: Always int.
+ */
+
+int *array_range(int min, int max)
+{
+	int *k, i;
+
+	if (min > max)
+		return (NULL);
+	k = malloc(sizeof(int) * (max - min + 1));
+	if (k == NULL)
+		return (NULL);
+	for (i = 0; i < (max - min) + 1; i++)
+		k[i] = min + i;
+	return (k);
+}
