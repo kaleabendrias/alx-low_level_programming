2e8a05b981a75f725f2d8d49a72a3822a6d537bc
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-17 06:48:00 -0700

    struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("(Name: (nill)\n");
+		printf("Name: (nill)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age <= 0)
