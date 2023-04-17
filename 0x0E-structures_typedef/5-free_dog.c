b64ceba995a530476152ff15a65aead2ce7addd2
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-17 09:40:40 -0700

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Entry point.
+ * @d: placeholder
+ * Return: 0
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
+
