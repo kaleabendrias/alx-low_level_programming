ce323d0e9f185a4622cba31646f90d602b6a8406
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-19 00:51:30 -0700

    fun pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - entry
+ * @name: name
+ * @f: pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
