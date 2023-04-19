083fcfd5758f9dcb82a7ef2a658bef7a4b6080db
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-19 01:31:04 -0700

    func pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints elements
+ * @array: array.
+ * @size: ho msny to print
+ * @action: hex or decimal.
+ * Return: void.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+	for (i = 0; i < aize; i++)
+		action(array[i]);
+}
+
