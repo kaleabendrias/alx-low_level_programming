eecaea5d5f9890f3f2994f6365448b75f63565b3
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-19 11:56:20 -0700

    hard

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - entry
+ * @argc: argumesnts
+ * @argv: pointer
+ * Return: Always
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2, a;
+	int (*res)(int, int);
+	char *get_op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	get_op = argv[2];
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = get_op_func(get_op);
+	a = res(n1, n2);
+
+	printf("%d\n", a);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: placeholder
+ * @b: placehilder
+ * Return: Always
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract
+ * @a: placeholder
+ * @b: placehilder
+ * Return: Always
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul
+ * @a: placeholder
+ * @b: placehoder
+ * Return: Always
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: placeholder
+ * @b: placeholder
+ * Return: Always
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: placholder
+ * @b: placeholder
+ * Return: Allways
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
