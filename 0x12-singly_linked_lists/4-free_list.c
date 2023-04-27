9b00f37186a4ccdbecb297995d6118836d469749
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-27 10:44:48 -0700

    fix

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free's list
+ * @head: struct
+ * Return: Always
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	list_t *current = head;
+	list_t *next;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
+
