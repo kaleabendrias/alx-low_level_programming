6560bbc7f3eb4bd2379ff639cc7613792762bf7a
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-28 00:58:25 -0700

    fix

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints elemetns
+ * @h: element
+ * Return: Always
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t c;
+	
+	c = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
+
