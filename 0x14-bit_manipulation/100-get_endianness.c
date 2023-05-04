b433d7ae0eb03961b5b1ab51b0caaf1eb0395c0d
kaleabendrias <kaleabendrias1212@gmail.com>
2023-05-04 11:16:14 -0700

    fix

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - gets endians
+ * Return: none
+ */
+
+int get_endianness(void)
+{
+	unsigned int i;
+
+	i = 1;
+	if (*(char *)&i == 1)
+		return (1);
+	else
+		return (0);
+}
+
