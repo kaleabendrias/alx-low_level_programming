367914742e8033c82da6c779741d304bea56dd8a
kaleabendrias <kaleabendrias1212@gmail.com>
2023-05-08 06:14:21 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,6 @@ int create_file(const char *filename, char *text_content)
 	wr = write(op, text_content, len);
 	if (op == -1 || wr == -1)
 		return (-1);
-	close (op);
+	close(op);
 	return (1);
 }
