bc73d52c6df5f405ec710728be9b6fd11ab0ec6f
kaleabendrias <kaleabendrias1212@gmail.com>
2023-05-08 05:48:53 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,21 +20,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buff = malloc(sizeof(char) * letters + 1);
 	if (buff == NULL)
 	{
-		close (fh);
+		close(fh);
 		return (0);
 	}
 	rd = read(fh, buff, letters);
 	if (rd == -1)
 	{
-		close (fh);
+		close(fh);
 		free(buff);
 		return (0);
 	}
 	buff[rd] = '\0';
-	wr = write(STDOUT_FILENO , buff, rd);
+	wr = write(STDOUT_FILENO, buff, rd);
 	if (wr == -1 || wr != rd)
 	{
-		close (fh);
+		close(fh);
 		free(buff);
 		return (0);
 	}
