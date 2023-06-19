534279dd6b085726383d57619ad6d231854ec5ab
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-19 00:30:35 -0700

    a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,51 @@
+/**
+ * add - add
+ * @a: first
+ * @b: second
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - add
+ * @a: first
+ * @b: second
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - add
+ * @a: first
+ * @b: second
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - add
+ * @a: first
+ * @b: second
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - add
+ * @a: first
+ * @b: second
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
