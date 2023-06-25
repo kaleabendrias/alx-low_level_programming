a0cd3873532c78ec7056e8f3a4322f0449683996
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-25 13:54:28 -0700

    add

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp 
+wget -P /tmp https://github.com/kaleabendrias/alx-low_level_programming/blob/fdd134aa1bac56b8106973d941cc69afcffdf84e/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
