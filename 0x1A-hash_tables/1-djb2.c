d9e1ddd1bf0f6b622ff4c6b41486d2a5c286b0f6
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-07 01:27:38 -0700

    fix

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 algrth
+ * @str: str
+ * Return: Always
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
