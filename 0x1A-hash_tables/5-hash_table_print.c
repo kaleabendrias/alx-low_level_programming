64467d28e9fe145a262f64db859903a34107c3a9
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-07 04:04:09 -0700

    fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,24 +10,24 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
-	 hash_node_t *node;
-	 int first;
+	hash_node_t *node;
+	int first = 1;
 
-	 first = 1;
-	 if (ht == NULL)
-		 return;
-	 printf("{");
-	 for (i = 0; i < ht->size; i++)
-	 {
-		 node = ht->array[i];
-		 while (node != NULL)
-		 {
-			 if (!first)
-				 printf(", ");
-			 printf("'%s': '%s'", node->key, node->value);
-			 first = 0;
-			 node = node->next;
-		 }
-	 }
-	 printf("}\n");
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
 }
