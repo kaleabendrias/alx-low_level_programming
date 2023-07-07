16f1b50a15abb65ae9879216135956697f3c2c4f
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-07 02:08:51 -0700

    fixed betty7

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,10 +39,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	if (new_node->key == NULL || new_node->value == NULL)
 	{
-		 free(new_node->key);
-		 free(new_node->value);
-		 free(new_node);
-		 return (0);
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
 	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
