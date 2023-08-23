678c756021656b89be07ed329060c1b185faa11e
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-23 12:24:23 +0300

    added task100

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,11 +4,9 @@
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in grid.
-    
     Args:
     grid (list of list of integers): The grid describing the island.
-
-    Returns: 
+    Returns:
     int: The perimeter of the island.
     """
 
@@ -17,14 +15,8 @@ def island_perimeter(grid):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
                 perimeter += 4
-                
-                #check if it is left neighbour
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
-
-                #check if uupper neighbour
                 if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 2
-    
     return perimeter
-
