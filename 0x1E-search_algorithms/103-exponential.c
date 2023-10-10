a33a91b6b4a79e850dfe05ebf6301016db429eed
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-10 12:23:34 +0300

    finished

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -11,27 +11,24 @@
 */
 int exponential_search(int *array, size_t size, int value)
 {
-    size_t low, high, mid, bound, i;
+	size_t low, high, mid, bound, i;
+
 	if (!array || size == 0)
 		return (-1);
-
 	if (array[0] == value)
 	{
 		printf("Value checked array[0] = [%d]\n", array[0]);
 		return (0);
 	}
-
 	bound = 1;
 	while (bound < size && array[bound] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
 		bound *= 2;
 	}
-
 	printf("Value found between indexes [%lu] and [%lu]\n", bound / 2, bound);
-	low= bound / 2;
+	low = bound / 2;
 	high = (bound < size - 1) ? bound : size - 1;
-
 	while (low <= high)
 	{
 		mid = (low + high) / 2;
