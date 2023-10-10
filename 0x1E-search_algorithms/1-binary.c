22e6ee3f7c99d9a9b8008d61a656eb488e7aca05
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-10 09:13:48 +0300

    finished

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,7 +11,7 @@
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0;
-	size_t right = size -1 ;
+	size_t right = size - 1;
 	size_t i;
 	size_t mid;
 
@@ -33,7 +33,7 @@ int binary_search(int *array, size_t size, int value)
 		if (array[mid] < value)
 			left = mid + 1;
 		else
-			right = mid -1;
+			right = mid - 1;
 	}
 	return (-1);
 }
