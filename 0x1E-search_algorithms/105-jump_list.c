a3d5bc308733a034f24b878cc9c0428eac0abf03
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-11 13:47:07 +0300

    finished

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,47 +1,44 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * jump_list - jump searches on singly linked list
- * @list: pointer to head node
- * @size: its size
- * @value: value to search for
- *
- * Return: the node found or NULL
- */
+* jump_list - jump searches a singly linked list
+* @list: pointer to head node
+* @size: size of the list
+* @value: value to search for
+*
+* Return: the node found or NULL
+*/
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t i = 0, j = sqrt(size), k = 0, last_j = 0;
-	listint_t *last = list;
+	size_t step = sqrt(size);
+	size_t i;
+	listint_t *current = list;
+	listint_t *prev = NULL;
 
 	if (!list)
 		return (NULL);
 
-	while (list->n < value)
+	while (current->n < value && current->next)
 	{
-		for (last_j = i, last = list, k = 0; list->next && k < j; k++)
-		{
-			list = list->next;
-			i++;
-		}
-		printf("Value checked at index [%lu] = [%d]\n", i, list->n);
-		if (!list->next)
-			break;
+		prev = current;
+		for (i = 0; i < step && current->next; i++)
+			current = current->next;
+
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
 	}
 
-	if (!list->next)
-		j = last_j;
-	else
-		j = i >= j ? i - j : 0;
-	printf("Value found between indexes [%lu] and [%lu]\n", j, i);
-	i = i >= size ? size - 1 : i;
-	list = last;
-	while (list)
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	prev ? prev->index : 0, current->index);
+
+	while (prev && prev->index < size && prev->n < value)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", j, list->n);
-		if (list->n == value)
-			return (list);
-		j++;
-		list = list->next;
+		prev = prev->next;
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
 	}
+
+	if (prev && prev->n == value)
+		return (prev);
+
 	return (NULL);
 }
